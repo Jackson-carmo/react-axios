89a0d784c1fa4fa7b61b6297145f542502895cf9
Jackson-carmo <jacksoncruciol157@gmail.com>
2022-12-03 09:54:58 -0300

    first commit

---
+++ src/router/Home.jsx
@@ -0,0 +1,46 @@
+import blogFetch from "../axios/config";
+
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+
+import "./Home.css";
+
+
+
+const Home = () => {
+
+  const [post, setPost] = useState([]);
+
+  const getPost = async() => {
+      try {
+        const response = await blogFetch.get("/posts")
+        const data = response.data;
+
+        setPost(data);
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  useEffect(() => {
+      getPost();
+  },[]);
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+    {post.length === 0 ? (<p>Carregando...</p>) : (
+      post.map((post) => (
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={`/post/${post.id}`} className="btn">Ler mais</Link>
+        </div>
+      ))
+    )}
+    </div>
+  )
+}
+
+export default Home
